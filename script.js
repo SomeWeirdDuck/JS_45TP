beb788db7bb8b5fb903bbc16c69c38b5dd56a72a
Kikos <73939491+SomeWeirdDuck@users.noreply.github.com>
2021-02-03 11:51:35 +0300

    Final version

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 window.addEventListener('load', function() {
-		
+        let button1 = document.getElementById('ff1');
+        let button2 = document.getElementById('ff2');
+        button1.addEventListener("click", function() {
+            window.location.href = "https://shrek-mult.ru/";
+        });
+    button2.addEventListener("click", function() {
+    var x= prompt('Введите цвет:', '');
+    document.body.style.background=x;
+});
 });
